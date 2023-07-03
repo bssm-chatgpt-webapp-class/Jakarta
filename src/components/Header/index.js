72fd59ed4152d6fbf2b61534f3b9557a40bcc75a
sebanimm <gks32114221@gmail.com>
2023-07-03 11:00:54 +0900

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { PlusIcon, HambugIcon } from "../../icons";
+
+const Header = () => {
+  return (
+    <div>
+      <PlusIcon />
+      <div>New Chat</div>
+      <HambugIcon />
+    </div>
+  );
+};
+
+export default Header;
